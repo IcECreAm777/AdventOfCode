c2789c60206a37311fbbef4668a3c73986c9321e
Henning <hgk.1998@googlemail.com>
2017-12-05 18:20:49 +0100

    finished day 5

--- day5/MainClass.java
+++ day5/MainClass.java
@@ -1,27 +1,60 @@
 package day5;
 
+import java.util.List;
+
+import allDays.InputReader;
+
 public class MainClass {
 
 	public static void main(String[] args) {
 		
 		System.out.println("Program started");
 		
-		String input = "1111";
+		InputReader ir = new InputReader();
+		List<String> input = ir.GetInput("./day5/day5_Input");
 		
 		System.out.println("Part 1 of Day 5");
-		int[] numbers = new int[input.length()];
+		int[] numbers = new int[input.size()];
 
-		for(int index = 0; index < input.length(); index++) {
-			numbers[index] = input.charAt(index) - '0';
+		for(int index = 0; index < input.size(); index++) {
+			numbers[index] = Integer.parseInt(input.get(index));
 		}
 
 		int index = 0;
 		int steps = 0;
 		while (index < numbers.length) {
-			index += ++numbers[index];
+			numbers[index]++;
+			index += numbers[index] - 1;
 			
 			steps++;
 		}
+		
+		System.out.println(steps);
+		
+		System.out.println("Part 2 of Day 5");
+		
+		/*reset Array*/
+		numbers = new int[input.size()];
+		for(int i = 0; i < input.size(); i++) {
+			numbers[i] = Integer.parseInt(input.get(i));
+		}
+		
+		index = 0;
+		steps = 0;
+		
+		while(index < numbers.length) {
+			if(numbers[index] >= 3) {
+				numbers[index]--;
+				index += numbers[index] + 1;
+			} else {
+				numbers[index]++;
+				index += numbers[index] - 1;
+			}
+			steps++;
+		}
+		
+		System.out.println(steps);
+		System.out.println("Program End");
 	}
 
 }
